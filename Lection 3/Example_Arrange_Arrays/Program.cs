e5ba1278c23a64bb109b6830ebaaf538d3d5ce8d
Markov Sergei <irbis_mssw@mail.ru>
2022-08-28 14:05:28 +0700

    Added notes on the third lecture

---
+++ Lection 3/Example_Arrange_Arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿// 1. Найти позицию минимального элемента в неотсортированной части массива.
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции.
+// 3. Повторять пока есть неотсортированные элементы.
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array) // - метод для вывода массива
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) // - метод упорядочевания массива
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for(int j = i + 1; j < array.Length; j++)               // - блок
+        {                                                       //   ищет
+            if(array[j] < array[minPosition]) minPosition = j;  //   максимальный
+        }                                                       //   элемент
+
+        int temporary = array[i];                               // - в этом блоке
+        array[i] = array[minPosition];                          //   производится
+        array[minPosition] = temporary;                         //   замена (свап)
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
---
+++ Lection 3/Exemple010_Methods/Program.cs
@@ -0,0 +1,53 @@
+﻿// Вид 1 (ничего не возвращает и ничего не принимает)
+void Method1()
+{
+    Console.WriteLine("Author - Markov Sergei");
+}
+Method1();
+
+
+// Вид 2 (ничего не возвращает, но может принимать какие-либо аргументы)
+void Method2(string msg)
+{
+    Console.WriteLine(msg); 
+}
+Method2("Текст сообщения");
+
+
+// Вид 3 (что-то возвращают, но ничего не принимают)
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4 (что-то принимает и что-то возвращает)
+// string Method4(int count, string text) - через while
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string result = Method4(3, "qwerty");
+// Console.WriteLine(result);
+
+
+string Method4(int count, string text) //- через for
+{
+    string result = String.Empty;
+
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string result = Method4(3, "qwerty");
+Console.WriteLine(result);
